bf4d5e626c7d931aaec359210bfa507f182af1b8
heiner <heiner.giehl@yahoo.com>
2022-06-26 07:53:53 +0200

    added db and app.use() stuff

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from "express"
-
+import dotenv from "dotenv"
+import cors from "cors"
+import { todoRoutes } from "./routes/todos"
+import { connectToDatabase } from "./db/db"
+dotenv.config()
 const app = express()
-
+app.use(express.json())
+app.use(cors())
 const PORT = process.env.PORT || 4000
 
+app.use(todoRoutes)
 app.get("/", (req, res) => res.send("Hello from server!"))
 
-app.listen(PORT, () =>
+app.listen(PORT, () => {
+  connectToDatabase()
   console.log(`⚡Server is running here 👉 https://localhost:${PORT}`)
-)
+})
